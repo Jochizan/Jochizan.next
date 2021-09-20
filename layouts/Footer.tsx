6a8855931fab2a3de346d6c42d18fc7963b26e8e
Jochizan <remnyachizot2015@gmail.com>
2021-09-19 20:23:04 -0500

    Fixed fonts

--- layouts/Footer.tsx
+++ layouts/Footer.tsx
@@ -3,7 +3,7 @@ import { memo } from 'react';
 const Footer = () => {
   return (
     <footer className='footer'>
-      <small className='text-base'>sitio diseñado por </small>
+      <small className='text-1rem'>sitio diseñado por </small>
       <a className='style-none' href='https://www.linkedin.com/in/jochizan'>
         @jochizan
       </a>
